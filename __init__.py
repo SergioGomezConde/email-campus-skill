9f047cee156bb36ef41029c3c1d18e464067fe46
serggom <sergio.s.gomezconde@gmail.com>
2022-06-25 19:34:19 +0200

    avances

--- __init__.py
+++ __init__.py
@@ -1,4 +1,39 @@
+import time
+
 from mycroft import MycroftSkill, intent_file_handler
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def inicio_sesion(self):
+    # Datos de acceso fijos
+    usuario = 'e71180769r'
+    contrasena = 'p5irZ9Jm4@9C#6WUaE!z9%@V'
+
+    # Modo headless
+    options = Options()
+    options.headless = True
+    options.add_argument("--windows-size=1920,1200")
+
+    self.speak("Buscando la informacion...")
+
+    # Acceso a pagina
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://campusvirtual.uva.es/login/index.php')
+
+    # Inicio de sesion
+    driver.find_element(by=By.NAME, value='adAS_username').send_keys(usuario)
+    driver.find_element(
+        by=By.NAME, value='adAS_password').send_keys(contrasena)
+    driver.find_element(by=By.NAME, value='adAS_submit').click()
+
+    # Aceptar cookies
+    driver.implicitly_wait(10)
+    driver.find_element(
+        by=By.XPATH, value='/html/body/div[1]/div/a[1]').click()
+
+    return driver
 
 
 class EmailCampus(MycroftSkill):
