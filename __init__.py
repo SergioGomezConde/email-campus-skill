525450ade20d8e404caf1a0c59d38ced143ebf0e
serggom <sergio.s.gomezconde@gmail.com>
2022-07-02 19:36:09 +0200

    Comprobación de existencia de fichero

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from mycroft import MycroftSkill, intent_file_handler
 
@@ -12,12 +13,19 @@ class EmailCampus(MycroftSkill):
 
     @intent_file_handler('campus.email.intent')
     def handle_campus_email(self, message):
+
         # Lectura de la informacion del fichero JSON
-        with open(ficheroJSON) as ficheroUsuario:
-            data = json.load(ficheroUsuario)
-            for user in data['usuario']:
-                self.speak(
-                    "Su dirección de correo electrónico de la Universidad de Valladolid es: " + user['email'])
+        if os.path.exists(ficheroJSON):
+
+            # Lectura de la informacion del fichero JSON
+            with open(ficheroJSON) as ficheroUsuario:
+                data = json.load(ficheroUsuario)
+                for user in data['usuario']:
+                    self.speak(
+                        "Su dirección de correo electrónico de la Universidad de Valladolid es: " + user['email'])
+
+        else:
+            self.speak("Lo siento, no dispongo de esa información")
 
 
 def create_skill():
